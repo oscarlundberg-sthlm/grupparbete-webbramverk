cb9cd21213eb1b204b2155332c775e1e4b868fc5
Igor Ljevak <igorljevak@gmail.com>
2021-04-02 23:02:35 +0200

    Products och single product design

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,107 +1,109 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../App.css';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-
-
 // ITEM render/function
-function Product( { item }) {
-    return (
-        
-        <Wrapper> 
-            <Link to={`/Item/${item['_id']}`}>
+function Product({ item }) {
+  return (
+    <Wrapper>
+      <Link to={`/Item/${item['_id']}`}>
         <Imgcontainer>
-            <img src={item.image} alt="logo" />    
-        </Imgcontainer>   
+          <img src={item.image} alt="logo" />
+        </Imgcontainer>
         <Infocontainer>
-        <TitleContainer>
-            <Title>{item.title}</Title>
-        </TitleContainer>
-            
-            <Description>{item.description.substring(0, 256)}</Description>
-            <Price><Span>$</Span>{item.price}</Price>
+          <TitleContainer>
+            <Title>{`${item.title.substring(0, 26)}`}</Title>
+          </TitleContainer>
+
+          <Description>{`${item.description.substring(0, 225)}...`}</Description>
+          <PriceDiv>
+            <Price>
+              <Span>$</Span>
+              {item.price}
+            </Price>
             <Stock>In stock: {item.stock}</Stock>
-            <Btn>ADD TO CART</Btn> 
+          </PriceDiv>
+
+          <Btn>ADD TO CART</Btn>
         </Infocontainer>
-        </Link>
-        </Wrapper>
-        
-    )
+      </Link>
+    </Wrapper>
+  );
 }
 
-export default Product
-
+export default Product;
 
 // Styling
 const Wrapper = styled.div`
-width: 23rem;
-height: 40rem;
-padding: 2rem;
-margin-bottom: 2.8rem;
-display: flex;
-flex-direction: column;
-align-items: center;
-border-radius: 1rem;
--moz-box-shadow:    3px 3px 5px 6px #ccc;
+  width: 18rem;
+  height: 26rem;
+  padding: 1rem;
+  margin-bottom: 2.8rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 1rem;
+  -moz-box-shadow: 3px 3px 5px 6px #ccc;
   -webkit-box-shadow: 3px 3px 5px 6px #ccc;
-  box-shadow:         3px 3px 5px 6px #ccc;
-`
-const Infocontainer = styled.div `
-height: 14rem;
-`
-const Imgcontainer = styled.div `
-height: 220px;
-width: 280px;
-margin: 0 auto;
-img {
+  box-shadow: 3px 3px 5px 6px #ccc;
+`;
+const Infocontainer = styled.div`
+  height: 14rem;
+`;
+const Imgcontainer = styled.div`
+  height: 140px;
+
+  margin: 0 auto;
+  img {
     position: relative;
     top: 50%;
     left: 50%;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
     max-width: 100%;
     max-height: 100%;
-}
-`
-const TitleContainer = styled.div `
-height: 3rem;
-`
-const Title = styled.h1 `
-color: #242424;
-font-size: 1.1rem;
-font-weight: 500;
-text-transform: uppercase;
-margin-top: 1.5rem;
-`
-const Description = styled.p `
-height: 9rem;
-font-size: .85rem;
-`
-const Price = styled.p `
-font-size: 2rem;
-color: #f34e1eca;
-margin-top: 1.5rem; 
-`
+  }
+`;
+const TitleContainer = styled.div`
+  margin-bottom: 0.4rem;
+`;
+const Title = styled.h1`
+  color: #242424;
+  font-size: 1rem;
+  font-weight: 500;
+  text-transform: uppercase;
+  margin-top: 0.8rem;
+`;
+const Description = styled.p`
+  font-size: 0.7rem;
+  height: 6rem;
+`;
+const PriceDiv = styled.div`
+  margin-top: 0.9rem;
+`;
+
+const Price = styled.p`
+  font-size: 1.5rem;
+  color: #f34e1eca;
+`;
 const Span = styled.span`
-color: #f34e1ebb; 
-font-size: 1.6rem;
-font-weight: 700;
-`
+  color: #f34e1ebb;
+  font-size: 1.2rem;
+  font-weight: 700;
+`;
 const Btn = styled.button`
-width: 18rem;
-height: 2.5rem;
-border: none;
-color: whitesmoke;
-background-color: #f34e1ebb;
-font-size: 1rem;
+  width: 16rem;
+  height: 2.5rem;
+  border: none;
+  color: whitesmoke;
+  background-color: #f34e1ebb;
+  font-size: 1rem;
 
-
-&:hover {
+  &:hover {
     background-color: #f34e1edd;
-}
-`
-const Stock = styled.p `
-font-size: .7rem;
-margin-left: .2rem;
-margin-bottom: 1rem;
-`
+  }
+`;
+const Stock = styled.p`
+  font-size: 0.65rem;
+  margin-left: 0.2rem;
+`;
--- src/pages/Item.js
+++ src/pages/Item.js
@@ -1,159 +1,156 @@
-import React, { useState, useEffect } from 'react'
-import Product from '../components/Product'
-import Pic from '../logo.svg'
-import styled from 'styled-components'
+import React, { useState, useEffect } from 'react';
+import Product from '../components/Product';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-
-
-
 function Item({ match }) {
-    const [singleItem, setSingleItem] = useState({});
+  const [singleItem, setSingleItem] = useState({});
 
-    const getItemData = async () => {
-        const response = await fetch(`http://localhost:5000/products/${match.params.id}`)
-        const data = await response.json();
-        
-        setSingleItem(data);
-    }
+  const getItemData = async () => {
+    const response = await fetch(`http://localhost:5000/products/${match.params.id}`);
+    const data = await response.json();
 
-    useEffect(() => {
-        getItemData();
-    }, [])
-        
-    
-    return (
-        <Main>
-        <Wrapper>
-            <Imgcontainer>
-            <img src={singleItem.image}alt="logo" width="400"/>  
-            </Imgcontainer>
-            <TextContainer>
-                <Closecontainer>
-                <Link to="/"><Closebtn>X</Closebtn></Link>
-                
-                    </Closecontainer>
-                
-           <Title >{singleItem.title}</Title>
-           <Price><Span>$</Span>{singleItem.price}</Price>
-           <Description>{singleItem.description}</Description>
-           <Btn>ADD TO CART</Btn>
-           <BtnH><i class="fa fa-heart"></i></BtnH>
-            <Stock>In stock: {singleItem.stock}</Stock>
-            
-           </TextContainer>
-           
-           </Wrapper>
-           </Main>
-    )
+    setSingleItem(data);
+  };
+
+  useEffect(() => {
+    getItemData();
+  }, []);
+
+  return (
+    <Main>
+      <Wrapper>
+        <Imgcontainer>
+          <img src={singleItem.image} alt="logo" width="400" />
+        </Imgcontainer>
+        <TextContainer>
+          <Closecontainer>
+            <Link to="/">
+              <Closebtn>X</Closebtn>
+            </Link>
+          </Closecontainer>
+
+          <Title>{singleItem.title}</Title>
+
+          <Description>{singleItem.description}</Description>
+          <Price>
+            <Span>$</Span>
+            {singleItem.price}
+          </Price>
+          <Btn>ADD TO CART</Btn>
+          <BtnH>
+            <i class="fa fa-heart"></i>
+          </BtnH>
+          <Stock>In stock: {singleItem.stock}</Stock>
+        </TextContainer>
+      </Wrapper>
+    </Main>
+  );
 }
 
-export default Item
-
-
+export default Item;
 
 // Styling
-const Main = styled.div `
-display: inline-block;
-position: relative;
-left: 50%;
-transform: translateX(-50%); 
-margin-top: 80px;
-height: 70vh;
-`
+const Main = styled.div`
+  display: inline-block;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  margin-top: 80px;
+  height: 70vh;
+`;
 const Wrapper = styled.div`
-display: flex; 
-justify-content: space-between;
-width: 70rem;
-height: 40rem;
-padding: 1.5rem;
-border-radius: 1rem;
--moz-box-shadow:    3px 3px 5px 6px #ccc;
+  display: flex;
+  justify-content: space-between;
+  width: 55rem;
+  height: 30rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  -moz-box-shadow: 3px 3px 5px 6px #ccc;
   -webkit-box-shadow: 3px 3px 5px 6px #ccc;
-  box-shadow:         3px 3px 5px 6px #ccc;
-`
-const Imgcontainer = styled.div `
-height: 400px;
-width: 300px;
-margin: 0 auto;
-img {
+  box-shadow: 3px 3px 5px 6px #ccc;
+`;
+const Imgcontainer = styled.div`
+  height: 350px;
+  width: 330px;
+  margin: 30px auto;
+  img {
     position: relative;
     top: 50%;
     left: 50%;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
     max-width: 100%;
     max-height: 100%;
-}
-`
+  }
+`;
 
-const TextContainer = styled.div `
-width: 30rem;
-`
-const Title = styled.h1 `
-color: #242424;
-margin-top: 1rem;
-font-size: 2.4rem;
-font-weight: 500;
-text-transform: uppercase;
-`
-const Price = styled.p `
-font-size: 3rem;
-color: #f34e1eca; 
-`
+const TextContainer = styled.div`
+  width: 25rem;
+`;
+const Title = styled.h1`
+  color: #242424;
+  margin-top: 1rem;
+  font-size: 1.4rem;
+  font-weight: 500;
+  text-transform: uppercase;
+`;
+const Price = styled.p`
+  font-size: 2rem;
+  color: #f34e1eca;
+`;
 const Span = styled.span`
-color: #f34e1ebb; 
-font-size: 2.3rem;
-font-weight: 700;
-`
-const Description = styled.p `
-margin-top: 2.3rem;
-font-size: 1.2rem;
-height: 15rem;
-`
-const Stock = styled.p `
-font-size: 1rem;
-margin: .4rem .2rem;
-
-`
+  color: #f34e1ebb;
+  font-size: 1.5rem;
+  font-weight: 700;
+`;
+const Description = styled.p`
+  margin-top: 1rem;
+  font-size: 0.8rem;
+  height: 12rem;
+`;
+const Stock = styled.p`
+  font-size: 0.8rem;
+  margin: 0.4rem 0.2rem;
+`;
 const Btn = styled.button`
-width: 20rem;
-height: 3rem;
-border: none;
-color: whitesmoke;
-background-color: #f34e1ebb;
-font-size: 1.3rem;
+  width: 20rem;
+  height: 3rem;
+  border: none;
+  color: whitesmoke;
+  background-color: #f34e1ebb;
+  font-size: 1.3rem;
 
-&:hover {
+  &:hover {
     background-color: #f34e1edd;
-}
-`
+  }
+`;
 const BtnH = styled.button`
-margin-left: .3rem;
-width: 4rem;
-height: 3rem;
-border: none;
-color: whitesmoke;
-background-color: #f34e1ebb;
-font-size: 1.3rem;
+  margin-left: 0.3rem;
+  width: 4rem;
+  height: 3rem;
+  border: none;
+  color: whitesmoke;
+  background-color: #f34e1ebb;
+  font-size: 1.3rem;
 
-&:hover {
+  &:hover {
     color: #f23030;
-    
+
     background-color: #f34e1edd;
-}
-`
+  }
+`;
 
-const  Closecontainer = styled.div `
-display:flex;
-justify-content: flex-end;
-`
+const Closecontainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
 
-const Closebtn = styled.button `
-background-color: #ec7979;
-border: none;
-color: whitesmoke;
-font-weight: 900;
-&:hover {
+const Closebtn = styled.button`
+  background-color: #ec7979;
+  border: none;
+  color: whitesmoke;
+  font-weight: 900;
+  &:hover {
     background-color: #e96262;
-}
-`
+  }
+`;
