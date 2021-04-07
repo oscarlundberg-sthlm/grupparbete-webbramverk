0b854dbcc33ceb329efa5ba0fe21d5efd19c1880
Igor Ljevak <igorljevak@gmail.com>
2021-04-07 18:22:17 +0200

    product anim

--- src/pages/Items.js
+++ src/pages/Items.js
@@ -1,26 +1,21 @@
-import React, { useState, useEffect } from 'react'
-import Product from '../components/Product'
-import {getAllItems} from '../functions/GetAllItems';
+import React, { useState, useEffect } from 'react';
+import Product from '../components/Product';
+import { getAllItems } from '../functions/GetAllItems';
 
-function Items( ) {
-    const [items, setItems] = useState([]);
+function Items() {
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        getAllItems().then(data => setItems(data));
-    }, []);
+  useEffect(() => {
+    getAllItems().then((data) => setItems(data));
+  }, []);
 
-
-    return (
-        <div className="itemsContainer">
-            
-            {
-                items.map( (item) => (
-                    <Product key={item['_id']} item={item}/>
-                ))
-            }
-            
-        </div>
-    )
+  return (
+    <div className="itemsContainer">
+      {items.map((item) => (
+        <Product key={item['_id']} item={item} />
+      ))}
+    </div>
+  );
 }
 
-export default Items
+export default Items;
